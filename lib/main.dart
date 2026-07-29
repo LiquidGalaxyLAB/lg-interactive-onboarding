89f11f987d6f09ea0c7d16539476d7e6e82c9e34
Darpan Baviskar <darpanbaviskar@gmail.com>
2026-07-29 16:25:24 +0530

    refactor(3d-models): delegate KMZ extraction to rig and remove local archive dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:lg_interactive_onboarding/src/app_shell.dart';
 import 'package:lg_interactive_onboarding/src/common/curriculum/guided_mode_controller.dart';
 import 'package:lg_interactive_onboarding/src/common/curriculum/learning_module.dart';
 import 'package:lg_interactive_onboarding/src/common/theme/app_theme.dart';
